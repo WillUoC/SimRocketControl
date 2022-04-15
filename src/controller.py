984691962758e993b7c23f5133eec176aa0a26e6
William Black <blackwl@mail.uc.edu>
2022-04-15 15:34:15 -0400

    Added boilerplate. PID code added.

---
+++ src/controller.py
@@ -0,0 +1,11 @@
+
+class Controller:
+    
+    def __init__(self_):
+        pass
+    
+    def cmd_loop(self, delay: float = 0.1):
+        pass
+
+    def set_gains(self, gains):
+        pass
--- src/rocket.py
+++ src/rocket.py
@@ -3,4 +3,31 @@
 class Rocket:
 
     def __init__(self):
-        return(None)
+        pass
+    
+    def _compute_body_matrix(self):
+        pass
+
+    def set_state(self, states):
+        pass
+
+    def set_deltas(self, deltas):
+        pass
+
+    def get_pos(self):
+        pass
+
+    def _update_state_array(self):
+        pass
+
+    def get_states(self):
+        pass
+
+    def get_state_array(self):
+        pass
+
+    def dynamics(self, t, x, states, deltas):
+        pass
+
+    def get_vertices(self):
+        pass
