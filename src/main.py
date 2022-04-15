f8ab5e4e8b9de1c7bd7be4b3766bfacfad512250
William Black <blackwl@mail.uc.edu>
2022-04-15 15:13:06 -0400

    Organization

--- src/main.py
+++ src/main.py
@@ -3,6 +3,9 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
 
+from rocket import *
+
+
 # Main Thread
 def main():
     # Put the code here!
---
+++ src/rocket.py
@@ -0,0 +1,6 @@
+
+
+class Rocket:
+
+    def __init__(self):
+        return(None)
